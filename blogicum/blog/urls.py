b2552206297cdbe7c72f6db35e050ccaa4be96e8
UMAtaullin <ataullinural@gmail.com>
2024-08-12 13:35:57 +0500

    extends and include templates

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-  path('', views.index, name='index'),
+  path('', views.index, name='home'),
   path('posts/<int:id>/', views.post_detail, name='post_detail'),
   path('category/<slug:category_slug>/', views.category_posts, name= 'category_post'),
 ]
