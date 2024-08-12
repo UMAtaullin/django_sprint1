ac7dec95491369597fb20027a1780e1655f97cc5
UMAtaullin <ataullinural@gmail.com>
2024-08-12 07:01:20 +0500

    urls done

---
+++ blogicum/blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+def post_detail(request, id):
+    return render(request, 'blog/detail.html')
+
+def category_posts(request, category_slug):
+    return render(request, 'blog/category.html')
